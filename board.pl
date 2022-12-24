dcb7d16a71bb030b7211d77ea399a7ae51a2619e
anasofia2407 <ana.sgvs.costa@gmail.com>
2022-12-24 15:17:02 +0000

    Update board.pl

--- board.pl
+++ board.pl
@@ -1,1 +1,11 @@
 
+[[empty,empty,empty,empty,piece(black,elephant),piece(black,elephant),empty,empty,empty,empty],
+    [empty,empty,empty,empty,piece(black,mouse),piece(black,mouse),piece(black,lion),empty,empty,empty],
+    [empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
+    [empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
+    [empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
+    [empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
+    [empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
+    [empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
+    [empty,empty,empty,piece(white,lion),piece(white,mouse),piece(white,mouse),piece(white,lion),empty,empty,empty],
+    [empty,empty,empty,empty,piece(white,elephant),piece(white,elephant),empty,empty,empty,empty]]
